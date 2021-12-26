af806515b47d04d907439b74c81e6efd887e33e8
jerry <309032663@qq.com>
2021-12-26 17:37:23 +0800

    update

--- notepad-server/src/main/java/cn/hsp/login/config/WebSecurityConfig.java
+++ notepad-server/src/main/java/cn/hsp/login/config/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 // 设置允许匿名访问路径
-                .antMatchers("/api/**", "/blog/**").permitAll()
+                .antMatchers("/api/**", "/note/**").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
     }
