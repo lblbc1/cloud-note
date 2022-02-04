6ad08b79b41481e1422fd360e94bb88ec3642796
jerry <309032663@qq.com>
2022-02-04 21:56:37 +0800

    update

--- notepad-server/src/main/resources/data.sql
+++ notepad-server/src/main/resources/data.sql
@@ -1,3 +1,29 @@
+CREATE TABLE `sys_user` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(255) NOT NULL,
+`password` varchar(255) NOT NULL,
+PRIMARY KEY (`id`)
+) ;
+
+CREATE TABLE `sys_role` (
+`id` int(11) NOT NULL,
+`name` varchar(255) NOT NULL,
+PRIMARY KEY (`id`)
+) ;
+
+CREATE TABLE `sys_user_role` (
+`user_id` int(11) NOT NULL,
+`role_id` int(11) NOT NULL,
+PRIMARY KEY (`user_id`,`role_id`)
+) ;
+
+CREATE TABLE `note` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `userId` int(11) unsigned NOT NULL,
+  `content` TEXT DEFAULT NULL,
+  `lastUpdateTime` DATETIME DEFAULT now(),
+  PRIMARY KEY (`id`)
+) ;
 
 INSERT INTO `sys_role` VALUES ('1', 'ROLE_ADMIN');
 INSERT INTO `sys_role` VALUES ('2', 'ROLE_USER');
