184748ca8989692e3047a26612ddc6024e582f68
jerry <309032663@qq.com>
2022-02-04 18:26:07 +0800

    update

--- notepad-vue/src/main.js
+++ notepad-vue/src/main.js
@@ -10,16 +10,12 @@ import router from './router'
 // 引入element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-// import echarts from 'echarts'
 import store from './store'
-// import '../theme/index.css'
 
 
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
-
-// Vue.prototype.$echarts = echarts
 
 new Vue({
   el:"#app",
