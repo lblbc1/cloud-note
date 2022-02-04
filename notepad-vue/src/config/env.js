a964e694de721bd7f314220d01fa39e16242ae0f
jerry <309032663@qq.com>
2022-02-04 17:27:42 +0800

    update

--- notepad-vue/src/config/env.js
+++ notepad-vue/src/config/env.js
@@ -5,18 +5,8 @@
  * 公众号：蓝不蓝编程
  */
 
-// 配置编译环境和线上环境之间的切换
+let baseUrl = 'http://localhost:8080/';
 
-let baseUrl = '';
-const env = process.env
-if (env.NODE_ENV == 'development') {
-    baseUrl = `http://localhost:8080/`; // 开发环境地址
-} else if (env.NODE_ENV == 'production') {
-    baseUrl = ``; //生产环境地址
-} else if (env.NODE_ENV == 'graph.vue') {
-    baseUrl = ``; //测试环境地址
-}
 export {
     baseUrl,
-    env
 }
--- notepad-vue/src/store/index.js
+++ notepad-vue/src/store/index.js
@@ -26,11 +26,9 @@ export default new Vuex.Store({
     },
 
     actions: {
-        //这个部分我暂时用不上
     },
 
     modules: {
-        //这里是我自己理解的是为了给全局变量分组，所以需要写提前声明其他store文件，然后引入这里
     }
 })
 
