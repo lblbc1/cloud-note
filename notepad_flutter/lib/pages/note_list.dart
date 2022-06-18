b28fb1f2c574b41011d7a3bf45b4b981c3bbe901
jerry <309032663@qq.com>
2022-06-18 22:09:04 +0800

    update

--- notepad_flutter/lib/pages/note_list.dart
+++ notepad_flutter/lib/pages/note_list.dart
@@ -8,7 +8,7 @@ import 'add_note.dart';
 import 'view_note.dart';
 
 /// 厦门大学计算机专业 | 前华为工程师
-/// 专注《零基础学编程系列》https://cxyxy.blog.csdn.net/article/details/121134634
+/// 专注《零基础学编程系列》  http://lblbc.cn/blog
 /// 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
 /// 公众号：蓝不蓝编程
 class NoteListPage extends StatelessWidget {
